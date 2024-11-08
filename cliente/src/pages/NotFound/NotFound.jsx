a3bc61a1d736546fd8232f31b07c6225b1b49e18
Danilo Pereira <contato.daniloapereira@gmail.com>
2024-11-08 02:27:28 -0300

    feat: adiciona página de erro 404
    Criação da página de erro 404 para exibir mensagem de página não encontrada.

--- cliente/src/pages/NotFound/NotFound.jsx
+++ cliente/src/pages/NotFound/NotFound.jsx
@@ -1,6 +1,29 @@
+import { Helmet } from 'react-helmet-async'
+import SITE_NAME from '../../config'
+import styles from './NotFound.module.css'
+import imagemNaoEncontrada from '../../assets/images/NotFound/NotFound.png'
+
 const NotFound = () => {
     return (
-        <h1>Página não encontrada</h1>
+        <>
+            <Helmet>
+                <title>404 | {SITE_NAME}</title>
+            </Helmet>
+            <div className={styles.container}>
+                <div className={styles.grid}>
+                    <div className={styles.row}>
+                        <div className={styles.caixaErro}>
+                            <h2>Oooops!</h2>
+                            <p>Não conseguimos encontrar a página que você está procurando...</p>
+                            <a href="/" className={styles.botaoCta} aria-label="Voltar para a página inicial">Voltar para o início</a>
+                        </div>
+                    </div>
+                    <div className={styles.row}>
+                        <img src={imagemNaoEncontrada} alt="Página não encontrada" className={styles.imagem} draggable="false" />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
