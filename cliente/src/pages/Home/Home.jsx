48c85dc0a7f2c0ff1c25a33dd0e46213235d2f78
Danilo Pereira <contato.daniloapereira@gmail.com>
2024-11-08 02:26:44 -0300

    feat: adiciona página home
    Implementação inicial da página principal do site com layout básico e estrutura inicial.

--- cliente/src/pages/Home/Home.jsx
+++ cliente/src/pages/Home/Home.jsx
@@ -1,7 +1,46 @@
+import { Helmet } from 'react-helmet-async'
+import SITE_NAME from '../../config'
+import styles from './Home.module.css'
+
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className={styles.container}>
+            <Helmet>
+                <title>{SITE_NAME}</title>
+            </Helmet>
+
+            <nav className={styles.navbar}>
+                <a href="/"><div className={styles.logo}>FusionCake</div></a>
+                <div className={styles.navItems}>
+                    <a href="#">Produto</a>
+                    <a href="#">Equipe</a>
+                    <a href="#">Contato</a>
+                </div>
+                <div className={styles.authButtons}>
+                    <button className={styles.signIn}>Entrar</button>
+                    <button className={styles.signUp}>Registrar</button>
+                </div>
+            </nav>
+
+            <main>
+                <section>
+                    <h1>Pedaços de Felicidade</h1>
+                    <p>A vida ganha mais cor e sabor quando cada pedaço é um momento doce para recordar</p>
+                    <button className={styles.experimente}>Experimente</button>
+                    <div className={styles.mensagens}>
+                        <span>Sabores únicos</span>
+                        <span>Amor em pedaços</span>
+                        <span>Frequência de Sabores</span>
+                        <span>Cardápio Doce</span>
+                    </div>
+                </section>
+            </main>
+
+            <footer className={styles.footer}>
+                <p>&copy; {new Date().getFullYear()} FusionCake. Todos direitos reservados.</p>
+            </footer>
+        </div>
     );
-}
+};
 
 export default Home
